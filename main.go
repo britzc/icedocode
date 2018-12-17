d311f8bdd0aeefbf5d8ea8a626b509462357d47f
Corneile Britz <corneile.britz@gmail.com>
2018-12-17 16:11:09 +1300

    linked to influxdb

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"os"
 	"text/template"
+	"time"
+
+	client "github.com/influxdata/influxdb/client/v2"
 )
 
 const version string = "6.0.1"
@@ -35,6 +38,11 @@ func main() {
 	natsURL := os.Getenv("NATS_HOST")
 	influxDBURL := os.Getenv("INFLUXDB_HOST")
 
+	fmt.Println("Connecting to InfluxDB")
+	if err := writeData(influxDBURL); err != nil {
+		fmt.Println(err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := &PageData{
 			Version:     version,
@@ -53,4 +61,51 @@ func main() {
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func writeData(influxDBHost string) (err error) {
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     fmt.Sprintf("http://%s:8086", influxDBHost),
+		Username: "jenkins",
+		Password: "secretpassword",
+	})
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  "icedoapp",
+		Precision: "s",
+	})
+	if err != nil {
+		return err
+	}
+
+	// Create a point and add to batch
+	tags := map[string]string{"cpu": "cpu-total"}
+	fields := map[string]interface{}{
+		"idle":   10.1,
+		"system": 53.3,
+		"user":   46.6,
+	}
+
+	pt, err := client.NewPoint("cpu_usage", tags, fields, time.Now())
+	if err != nil {
+		return err
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		return err
+	}
+
+	// Close client resources
+	if err := c.Close(); err != nil {
+		return err
+	}
+
+	return nil
 }
